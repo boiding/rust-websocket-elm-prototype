60ec5e1e036f165c6ab654bde7c98cb696b2f7d1
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2017-11-15 20:14:57 +0100

    Build client.js and use websocket_address port

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,6 @@
 (function(Elm){
     var container = document.getElementById('container');
     var app = Elm.Main.embed(container);
+
+    app.ports.websocket_address.send('ws://127.0.0.1:3436');
 })(Elm);
