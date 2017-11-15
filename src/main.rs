a9f6c195eeb56e698c9392eda6a16a45b91fad5f
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2017-11-15 15:57:20 +0100

    Create a web socket thread

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,23 @@
-#[macro_use] extern crate log;
-extern crate simplelog;
-extern crate logger;
-extern crate iron;
-extern crate staticfile;
-extern crate mount;
 extern crate dotenv;
+extern crate iron;
+#[macro_use] extern crate log;
+extern crate logger;
+extern crate mount;
+extern crate simplelog;
+extern crate staticfile;
+extern crate ws;
 
 use std::env;
-use dotenv::dotenv;
 use std::path::Path;
 use std::thread;
 
+use dotenv::dotenv;
 use iron::{Iron, Chain};
-use staticfile::Static;
+use logger::Logger;
 use mount::Mount;
 use simplelog::{Config, LogLevelFilter, TermLogger, CombinedLogger};
-use logger::Logger;
-
+use staticfile::Static;
+use ws::listen;
 
 fn main() {
     dotenv().ok();
@@ -29,13 +30,36 @@ fn main() {
     info!("Logger configured");
 
     let iron_thread = thread::spawn(||{
-        let address = env::var("address").expect("\"address\" in environment variables");
-        info!("starting server at {}", address);
+        let server_address = env::var("address").expect("\"address\" in environment variables");
+        info!("starting server at {}", server_address);
 
-        Iron::new(chain()).http(address).unwrap();
+        Iron::new(chain()).http(server_address).unwrap();
+    });
+
+    let ws_thread = thread::spawn(||{
+        let socket_address = env::var("socket").expect("\"socket\" in environment variables");
+        info!("starting web socket at {}", socket_address);
+
+        if let Err(error) = listen(socket_address, |out| {
+
+            // The handler needs to take ownership of out, so we use move
+            move |msg| {
+
+                // Handle messages received on this connection
+                info!("Server got message '{}'. ", msg);
+
+                // Use the out channel to send messages back
+                out.send(msg)
+            }
+
+        }) {
+            // Inform the user of failure
+            println!("Failed to create WebSocket due to {:?}", error);
+}
     });
 
     iron_thread.join();
+    ws_thread.join();
 }
 
 fn chain() -> Chain {
